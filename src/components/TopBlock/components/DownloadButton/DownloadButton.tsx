a5f1e8bbe7379ae692d0a5f40f38acb5a27c2257
viktor <komarov.v@triadasite.ru>
2025-10-07 10:02:28 +0300

    fix hover

--- src/components/TopBlock/components/DownloadButton/DownloadButton.tsx
+++ src/components/TopBlock/components/DownloadButton/DownloadButton.tsx
@@ -12,25 +12,27 @@ interface DownloadButtonProps {
 export function DownloadButton({buttonData, icon}: DownloadButtonProps) {
     return (
         <Link href='/' className={styles.root}>
-            <div className={styles.before}>
-                <Image
-                    src={icon}
-                    alt=""
-                    width={24}
-                    height={24}
-                    priority
-                />
-            </div>
-            <div className={styles.text}>
-                {buttonData.label}
-            </div>
-            <div className={styles.after}>
-                <Image
-                    src="/assets/downloadIcon.svg"
-                    alt=""
-                    width={24}
-                    height={24}
-                />
+            <div className={styles.textWrap}>
+                <div className={styles.before}>
+                    <Image
+                        src={icon}
+                        alt=""
+                        width={24}
+                        height={24}
+                        priority
+                    />
+                </div>
+                <div className={styles.text}>
+                    {buttonData.label}
+                </div>
+                <div className={styles.after}>
+                    <Image
+                        src="/assets/downloadIcon.svg"
+                        alt=""
+                        width={24}
+                        height={24}
+                    />
+                </div>
             </div>
         </Link>
     )
